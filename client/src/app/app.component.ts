049bc9748a7de1da3869fcf0132655a17021fc6f
Unstable Molecule <33104287+unstablemolecule@users.noreply.github.com>
2023-05-16 12:10:43 -0700

    feat(client): add forecast pre block

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-dotnet-gh-actions';
+export class AppComponent implements OnInit {
+    constructor(private http: HttpClient) { }
+
+    forecast: any;
+
+    ngOnInit(): void {
+        this.getWeatherForecast();
+    }
+
+    getWeatherForecast() {
+        this.http.get('http://localhost:5000/api/WeatherForecast')
+            .subscribe(forecast => this.forecast = forecast);
+    }
+
+    title = 'angular-dotnet-gh-actions';
 }
