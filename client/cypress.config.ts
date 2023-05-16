936740bcf84093c61921dc947a4ba2de80d47c61
Unstable Molecule <33104287+unstablemolecule@users.noreply.github.com>
2023-05-16 12:13:32 -0700

    test(client): add should have forecast test

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
@@ -6,5 +6,8 @@ export default defineConfig({
             // implement node event listeners here
         },
         baseUrl: 'http://localhost:4200',
+        env: {
+            apiUrl: 'http://localhost:5000/api'
+        }
     },
 });
--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,5 +1,14 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+    it('passes', () => {
+        cy.visit('https://example.cypress.io');
+    });
+
+    it('should have forecast pre block', () => {
+        cy.intercept('GET', `${Cypress.env('apiUrl')}/WeatherForecast`).as('getWeatherForecast');
+        cy.visit('/');
+        cy.wait('@getWeatherForecast').then(() => {
+            cy.get(`[data-cy="forecast"]`)
+                .should('not.be.empty');
+        });
+    });
 })
