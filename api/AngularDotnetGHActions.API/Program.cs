6c9fd3129c227930266462237922f675e36d10fc
Unstable Molecule <33104287+unstablemolecule@users.noreply.github.com>
2023-05-16 12:10:00 -0700

    chore(api): configure api to be callable

--- api/AngularDotnetGHActions.API/Program.cs
+++ api/AngularDotnetGHActions.API/Program.cs
@@ -13,6 +13,17 @@ public class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        // Add CORS
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.WithOrigins(
+                    "http://localhost:4200"
+                );
+            });
+        });
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -22,10 +33,9 @@ public class Program
             app.UseSwaggerUI();
         }
 
-        app.UseHttpsRedirection();
+        app.UseCors();
 
         app.UseAuthorization();
-
 
         app.MapControllers();
 
